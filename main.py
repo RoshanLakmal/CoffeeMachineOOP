a019f399c8a0abecbe881ab131cbebc33d525818
Roshan <roshanrox911@gmail.com>
2023-08-26 22:42:48 +1000

    clean up

--- main.py
+++ main.py
@@ -2,19 +2,26 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee_menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
 def user_commands(user_input):
-    coffee_menu = Menu()
-    coffee_maker = CoffeeMaker()
+
 
     if user_input == "off":
         exit()
     elif user_input == "report":
         coffee_maker.report()
+        money_machine.report()
     else:
         drink = coffee_menu.find_drink(user_input)
 
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
 def start_coffee_machine():
-    coffee_menu = Menu()
     user_command = input(f"What would you like? ({coffee_menu.get_items()}): ")
     user_commands(user_command)
     start_coffee_machine()
